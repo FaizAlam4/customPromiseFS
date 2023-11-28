94a44064ac1b597858292f83441154b457c221eb
FaizAlam4 <faizalam4e@gmail.com>
2023-11-28 06:21:22 +0530

    all done with custom promise

--- fs-problem2.cjs
+++ fs-problem2.cjs
@@ -1,69 +1,133 @@
-const fs = require("fs").promises;
+const { rejects } = require("assert");
+const fs = require("fs");
 
 let myFunc = () => {
   let appendMyFile = (filePath, nameOfFile) => {
-    fs.appendFile(filePath, nameOfFile)
-      .then(() => {
-        return fs.readFile("./newLipsum.txt", "utf-8");
-      })
+    var p4 = new Promise((resolve, reject) => {
+      fs.appendFile(filePath, nameOfFile, (err) => {
+        if (err) reject(err);
+        else {
+          resolve();
+        }
+      });
+    });
+    p4.then(() => {
+      return new Promise((resolve, reject) => {
+        fs.readFile("./newLipsum.txt", "utf-8", (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        });
+      });
+    })
       .then((data) => {
         data = data.toLowerCase();
+        // console.log(data)
         let arr = data.split(".");
         arr = arr.join("\n");
-        return fs.writeFile("./newLipsum2.txt", arr);
+        return new Promise((resolve, reject) => {
+          fs.writeFile("./newLipsum2.txt", arr, (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
       })
       .then(() => {
-        return fs.appendFile("./fileName.txt", "./newLipsum2.txt");
+        return new Promise((resolve, reject) => {
+          fs.appendFile("./fileName.txt", "./newLipsum2.txt", (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
       })
       .then(() => {
-        return fs.readFile("./newLipsum2.txt", "utf-8");
+        return new Promise((resolve, reject) => {
+          fs.readFile("./newLipsum2.txt", "utf-8", (err,data) => {
+            if (err) reject(err);
+            else resolve(data);
+          });
+        });
       })
       .then((data) => {
         let dataArray = data.split("\n");
         dataArray.sort();
         let myData = dataArray.join("\n").trim();
-        return fs.writeFile("./newLipsum3.txt", myData);
+        return new Promise((resolve, reject) => {
+          fs.writeFile("./newLipsum3.txt", myData, (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
       })
       .then(() => {
-        return fs.appendFile("./fileName.txt", "./newLipsum3.txt");
+        return new Promise((resolve, reject) => {
+          fs.appendFile("./fileName.txt", "./newLipsum3.txt", (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
       })
       .then(() => {
-        return fs.readFile("./fileName.txt", "utf-8");
+        return new Promise((resolve, reject) => {
+          fs.readFile("./fileName.txt", "utf-8", (err,data) => {
+            if (err) reject(err);
+            else resolve(data);
+          });
+        });
       })
       .then((data) => {
         data = data.split("./");
-        console.log(data);
+        // console.log(data);
         for (let myData of data) {
           if (myData != "newLipsum.txt" && myData != "") {
-            fs.unlink(myData).catch((err) => {
-              console.log(err);
+            let myPro = new Promise((resolve, reject) => {
+              fs.unlink(myData, (err) => {
+                if (err) reject(err);
+                else resolve();
+              });
             });
+
+            myPro.catch((err) => console.log(err));
           }
         }
       })
       .catch((err) => {
-        console.log("Error:", err);
+        console.log(err);
       });
   };
+
   let writeMyFile = (name, data) => {
-    fs.writeFile(name, data)
-      .then(() => {
-        appendMyFile("./fileName.txt", name);
-      })
-      .catch((err) => {
-        console.log("Error:", err);
+    let p2 = new Promise((resolve, reject) => {
+      fs.writeFile(name, data, (err) => {
+        if (err) reject(err);
+        else {
+          resolve();
+        }
       });
+    });
+    p2.then(() => {
+      appendMyFile("./fileName.txt", name);
+    }).catch((err) => {
+      console.log("Error:", err);
+    });
   };
+
   let readMyFile = (fileNamePath) => {
-    fs.readFile(fileNamePath, "utf-8")
-      .then((data) => {
-        data = data.toUpperCase();
-        // console.log(data)
-        writeMyFile("./newLipsum.txt", data);
-      })
-      .catch((err) => {
-        console.log("Error:", err);
+    let p1 = new Promise((resolve, reject) => {
+      fs.readFile(fileNamePath, "utf-8", (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
       });
+    });
+    p1.then((data) => {
+      data = data.toUpperCase();
+
+      writeMyFile("./newLipsum.txt", data);
+    }).catch((err) => {
+      console.log("Error:", err);
+    });
   };
   readMyFile("./givenContent/lipsum.txt");
 };
